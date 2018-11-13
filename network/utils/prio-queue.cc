1c2c1c832d69635dfdd6db92877a6f5d0d291a3f
Stephen Ibanez <sibanez@g.hmc.edu>
2018-11-13 01:24:27 -0800

    Forgot to add prio-queue.cc to repo

---
+++ network/utils/prio-queue.cc
@@ -0,0 +1,30 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2018 Stanford University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Stephen Ibanez <sibanez@stanford.edu>
+ */
+
+#include "ns3/queue-item.h"
+#include "prio-queue.h"
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("PrioQueue");
+
+NS_OBJECT_TEMPLATE_CLASS_DEFINE (PrioQueue,QueueDiscItem);
+
+} // namespace ns3
